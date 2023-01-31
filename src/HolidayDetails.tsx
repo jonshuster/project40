338baa1e3eba07cf5001f2240b7e1d73170e0232
jonshuster <jonshuster@users.noreply.github.com>
2023-01-31 20:36:48 +0100

    Update prices

--- src/HolidayDetails.tsx
+++ src/HolidayDetails.tsx
@@ -126,11 +126,11 @@ const HolidayDetails = () => {
                         </div>
                         {/* Details - Prices*/}
                         <h2 className="major">Prices</h2>
-                        <p>These are obviously somewhat variable depending on when we book and whats available. Accommodation is the largest cost, some will be able to stay at mine, as soon as I know who's coming I'll look to arrange some group accommodation. Based on recent availability, these prices should be enough to give you an idea for now:</p>
+                        <p>These are obviously somewhat variable depending on when we book. Accommodation is the largest cost, some will be able to stay at mine, as soon as I know who's coming I'll look to arrange some group accommodation. Based on recent availability, these prices should be enough to give you an idea:</p>
                         <h3>Wed - Sun:</h3>
                         <ul>
                             <li>Flights: £80 &nbsp;<i>(Return Gatwick Easyjet)</i></li>
-                            <li>Accommodation: <CurrencyValue rates={exchangeRates} currency='€' amount={300} /> &nbsp;<i>(Based on two sharing, some including continental breakfast)</i></li>
+                            <li>Accommodation: <CurrencyValue rates={exchangeRates} currency='€' amount={320} /> &nbsp;<i>(Based on two sharing, including continental breakfast)</i></li>
                             <li>Transfers: <CurrencyValue rates={exchangeRates} currency='€' amount={20} /> to <CurrencyValue rates={exchangeRates} currency='€' amount={80} /> &nbsp;<i>(Range from public transport to private transfer)</i></li>
                         </ul>
                         <h3>Activities</h3>
